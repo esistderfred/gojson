c012aea6d7a6a62d932253d741b6ca7aba2cf5a2
Fred <esistderfred@gmx.de>
2021-03-19 20:32:18 +0100

    interface arrays

--- testdata/example.go
+++ testdata/example.go
@@ -4,19 +4,21 @@ import "time"
 
 // VENDOR implements json structure for vendor
 type VENDOR struct {
+	Name     string    `json:"name,omitempty"`
 	Url      string    `json:"url,omitempty"`
 	Added    time.Time `json:"added,omitempty"`
 	Priority int       `json:"priority,omitempty"`
-	Name     string    `json:"name,omitempty"`
 }
 
 // EXAMPLE implements json structure for example
 type EXAMPLE struct {
-	Timestamp time.Time              `json:"timestamp,omitempty"`
-	Children  map[string]interface{} `json:"children,omitempty"`
-	Vendor    *VENDOR                `json:"vendor,omitempty"`
-	Nullvalue interface{}            `json:"nullvalue,omitempty"`
-	Name      string                 `json:"name,omitempty"`
-	Number    int                    `json:"number,omitempty"`
-	Weight    float64                `json:"weight,omitempty"`
+	Number           int                    `json:"number,omitempty"`
+	Weight           float64                `json:"weight,omitempty"`
+	AlternativeNames []interface{}          `json:"alternative_names,omitempty"`
+	Order            []interface{}          `json:"order,omitempty"`
+	Name             string                 `json:"name,omitempty"`
+	Timestamp        time.Time              `json:"timestamp,omitempty"`
+	Children         map[string]interface{} `json:"children,omitempty"`
+	Vendor           *VENDOR                `json:"vendor,omitempty"`
+	Nullvalue        interface{}            `json:"nullvalue,omitempty"`
 }
